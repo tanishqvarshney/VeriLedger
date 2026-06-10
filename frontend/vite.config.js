70dd4335b6bb1234d8cda20149577eb444cb7347
Tanishq Varshney <tanishqvarshney@Tanishqs-MacBook-Air.local>
2026-06-10 12:07:03 +0530

    Support dynamic base path for dev and production builds in vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,14 +3,16 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/VeriLedger/',
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
-  server: {
-    host: '0.0.0.0',
-    port: 3000
-  }
+export default defineConfig(({ command }) => {
+  return {
+    base: command === 'serve' ? '/' : '/VeriLedger/',
+    plugins: [
+      react(),
+      tailwindcss()
+    ],
+    server: {
+      host: '0.0.0.0',
+      port: 3000
+    }
+  };
 })
